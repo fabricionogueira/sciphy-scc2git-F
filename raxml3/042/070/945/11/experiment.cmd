c6dc786b2f97ff93d24c870ea76e382374b1ecd9
root <root@ip-172-31-2-33.ec2.internal>
2016-11-22 14:52:36 -0500

    raxml3_commit

---
+++ raxml3/042/070/945/11/experiment.cmd
@@ -0,0 +1,8 @@
+#!/bin/bash
+# python /root/wf-scc2git/../bin/execute_raxml.py . /mnt/scc2git/exp/raxml3/042/070/945/11/vi.phylip /mnt/scc2git/exp/raxml3/042/070/945/11/vi.mg.modelFromMG.txt 100 4 3
+python /root/wf-scc2git/bin/execute_raxml.py . /mnt/scc2git/exp/raxml3/042/070/945/11/vi.phylip /mnt/scc2git/exp/raxml3/042/070/945/11/vi.mg.modelFromMG.txt 4 4 3
+
+/opt/jdk1.8.0_101/bin/java -jar /root/wf-scc2git/bin/SciPhyTreeGenerator.jar RAxML_bipartitions.`basename /mnt/scc2git/exp/raxml3/042/070/945/11/vi`.phylip_tree3.BS_TREE `basename /mnt/scc2git/exp/raxml3/042/070/945/11/vi`.tree
+
+echo "PHYTREE" >> ERelation.txt
+echo RAxML_bipartitions.`basename /mnt/scc2git/exp/raxml3/042/070/945/11/vi`.phylip_tree3.BS_TREE >> ERelation.txt
