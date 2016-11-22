f0ad97e09e0b9b7d1a70cff67a17968dffcb3b67
root <root@ip-172-31-59-46.ec2.internal>
2016-11-22 14:48:08 -0500

    raxml1_commit

---
+++ raxml1/651/836/488/0/experiment.cmd
@@ -0,0 +1,7 @@
+#!/bin/bash
+# python /root/wf-scc2git/../bin/execute_raxml.py . /mnt/scc2git/exp/raxml1/651/836/488/0/ii.phylip /mnt/scc2git/exp/raxml1/651/836/488/0/ii.mg.modelFromMG.txt 100 4 1
+python /root/wf-scc2git/bin/execute_raxml.py . /mnt/scc2git/exp/raxml1/651/836/488/0/ii.phylip /mnt/scc2git/exp/raxml1/651/836/488/0/ii.mg.modelFromMG.txt 4 4 1
+
+python /root/wf-scc2git/bin/extractor.py `basename /mnt/scc2git/exp/raxml1/651/836/488/0/ii` PA_Raxml1 ERelation.txt
+
+/opt/jdk1.8.0_101/bin/java -jar /root/wf-scc2git/bin/SciPhyExtractor.jar raxml RAxML_info.`basename /mnt/scc2git/exp/raxml1/651/836/488/0/ii`.phylip_raxml_tree1.singleTree ERelation.txt
